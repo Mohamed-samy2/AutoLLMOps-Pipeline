c0f1b0448341bb6db22d84393c49386c965fe6e0
Mohamed-samy2 <mohamedsamyy02@gmail.com>
2025-07-31 00:38:37 +0300

    Add environment variable configuration and update settings class attributes to uppercase

--- src/config/config.py
+++ src/config/config.py
@@ -2,10 +2,23 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     
-    target_domain:str
-    use_case:str
-    data_sources_path:str
-    base_llm:str
+    TARGET_DOMAIN:str
+    USE_CASE:str
+    DATA_SOURCES_PATH:str
+    BASE_LLM:str
+
+    # ============================ Database connection ===========================
+    DB_BACKEND:str
+    POSTGRES_USERNAME:str
+    POSTGRES_PASSWORD:str
+    POSTGRES_HOST:str
+    POSTGRES_PORT:int
+    POSTGRES_DB:str
+
+    # ============================ LLM Config ============================
+    LLM_PROVIDER:str
+    LLM_MODEL_ID:str
+    API_KEY:str
     
     class Config:
         env_file = ".env"
